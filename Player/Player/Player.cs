a6f6f445874d06ffffe5df024f8d43e15a448302
MillerPatrick214 <149214352+MillerPatrick214@users.noreply.github.com>
2025-10-21 15:06:43 -0700

    Initial Commit

---
+++ Player/Player/Player.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+using Godot.Collections;
+
+public partial class Player : CharacterBody3D
+{
+    Vector2 MouseCoords = Vector2.Zero;
+    public override void _Ready()
+    {
+        Vector2 Resolution = GetViewport().GetVisibleRect().Size; //This needs to be elsewhere eventually. This will change if viewport size changes during gameplay\
+        
+    }
+
+    public override void _Process(double delta)
+    {
+        MouseCoords = GetViewport().GetMousePosition();
+        
+    }
+
+
+}
