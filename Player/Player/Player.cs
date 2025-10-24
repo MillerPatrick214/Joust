dffe8fc1130270057270fed814593cf2a392360b
Chase H <chasehallahan@gmail.com>
2025-10-23 22:17:12 -0700

    Initial hand movement and stuff

--- Player/Player/Player.cs
+++ Player/Player/Player.cs
@@ -4,18 +4,44 @@ using Godot.Collections;
 
 public partial class Player : CharacterBody3D
 {
-    Vector2 MouseCoords = Vector2.Zero;
+
+    [Export] public int Speed { get; set; } = 14;
+    [Export] public int FallAcceleration { get; set; } = 75;
+
+    [Export] public RigidBody3D HeldItem;
+
+    private Vector3 _targetVelocity = Vector3.Zero;
+    private Vector2 _mouse;
+
     public override void _Ready()
     {
         Vector2 Resolution = GetViewport().GetVisibleRect().Size; //This needs to be elsewhere eventually. This will change if viewport size changes during gameplay\
-        
+        Input.MouseMode = Input.MouseModeEnum.Confined;
     }
 
-    public override void _Process(double delta)
+    public override void _Input(InputEvent @e)
     {
-        MouseCoords = GetViewport().GetMousePosition();
-        
+        if (@e.IsActionPressed("pause")) GetTree().Quit();
+
     }
 
+    public override void _PhysicsProcess(double delta)
+    {
+        // Ground and Vertical Movement
+        var direction = Vector3.Zero;
+
+        if (Input.IsActionPressed("move_right")) direction.X += 1.0f;
+        if (Input.IsActionPressed("move_left")) direction.X -= 1.0f;
+        if (Input.IsActionPressed("move_back")) direction.Z += 1.0f;
+        if (Input.IsActionPressed("move_forward")) direction.Z -= 1.0f;
+
+        _targetVelocity.X = direction.X * Speed;
+        _targetVelocity.Z = direction.Z * Speed;
+
+        if (!IsOnFloor()) _targetVelocity.Y -= FallAcceleration * (float)delta;
+
+        Velocity = _targetVelocity;
+        MoveAndSlide();
+    }
 
 }
